07f60e32108d9b7552787f8c81f48d0d5c1f5df5
Ainur <ismy1286@gmail.com>
2021-12-23 15:12:45 +0600

    first commit

--- ThirdTask/ViewController.swift
+++ ThirdTask/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class ViewController: UIViewController {
 
+    private var label = UILabel()
+    private var textField = UITextField()
+    private var button = UIButton()
+    
+    var s = "Айбек, Мирбек, Daniel, Erjan"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.text = s.replacingOccurrences(of: ", ", with: "\n", options: .literal, range: nil)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(100)
+            make.width.equalToSuperview().offset(-100)
+        }
+        
+        textField.placeholder = "Введите имя..."
+        
+        view.addSubview(textField)
+        textField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-120)
+            make.width.equalToSuperview().offset(-100)
+        }
+        
+        button.backgroundColor = .yellow
+        button.setTitle("Сохранить", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(nil, action: #selector(clickButton(sender:)), for: .touchUpInside)
+        
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-70)
+            make.width.equalTo(100)
+        }
+        
     }
-
-
+    @objc func clickButton(sender: UIButton) {
+        
+        s.append(", ")
+        s.append(textField.text ?? "")
+        textField.text = ""
+        label.text = s.replacingOccurrences(of: ", ", with: "\n", options: .literal, range: nil)
+    }
 }
-
